93bfcd18412584d23a558d2ad1029c45d6a18aaa
taisleal <tais.almeida.dev@gmail.com>
2026-03-12 02:29:40 -0300

    Initial commit

---
+++ src/main/java/com/nhl/nhl_spring_app/NhlSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.nhl.nhl_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhl/nhl_spring_app/NhlSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhl.nhl_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
